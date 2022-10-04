6eabe0ac26467f59709ff9fbf2fc9647a75d52a2
EC2 Default User <ec2-user@ip-172-31-2-105.ap-northeast-1.compute.internal>
2022-10-04 08:17:05 +0000

    [Update]Ruby8復習

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,3 +1,4 @@
+# 自分で作った解答
 def fizz_buzz(number)
   if (number%3 == 0 && number%5 != 0)
     return "Fizz"
@@ -19,3 +20,22 @@ input = gets.to_i
 
 puts "結果は…"
 puts fizz_buzz(input)
+
+# お手本の解答(メソッド部分のみ)
+=begin
+
+def fizz_buzz(number)
+ if number % 15 ==0
+   "FizzBuzz"
+ elseif number % 3 == 0
+   "Fizz"
+ elseif number % 5 ==0
+   "Buzz"
+ else
+   number.to_s
+ end
+end
+
+if~elseif~…elseはちゃんと順番通り評価、中断されるのでアクションスクリプトの時の疑心暗鬼記述しなくて良い。
+
+=end
--- greeting.rb
+++ greeting.rb
@@ -4,3 +4,17 @@ def greeting(name)
 end
 
 puts greeting('John')
+
+# 戻り値はdef~endの最後の式もしくはreturnを指定された部分になる。
+# returnが優先される。
+
+# 便利なリターン
+def clac(num)
+  return "計算できません" if num.zero?
+  return num*2
+end
+
+puts clac(3)
+
+# この例だとnumが0の時は"計算できません"が返され以降の処理が行われない。
+# 0以外の時は14行目の処理が行われる。
