997eef2974414bfd3444150356db4cac30ce54f9
EC2 Default User <ec2-user@ip-172-31-2-105.ap-northeast-1.compute.internal>
2022-10-04 06:14:20 +0000

    [UPdate]Ruby6復習と演習

---
+++ lesson6-1.rb
@@ -0,0 +1,16 @@
+puts "2つの値を入力してください"
+int1 = gets.to_i
+int2 = gets.to_i
+puts "計算結果を出力します"
+
+# 回答1
+puts "#{int1}*#{int2}=#{int1 * int2}"
+
+# 回答2
+# puts int1.to_s + "*" + int2.to_s + "=" + (int1 * int2).to_s
+
+# 回答3
+# int3 = int1 * int2
+# puts "#{int1}*#{int2}=#{int3}"
+
+puts "計算を終了します"
---
+++ lesson6-2.rb
@@ -0,0 +1,56 @@
+puts "計算をはじめます"
+puts "何回計算を繰り返しますか？"
+
+# 自分で考えた処理
+
+r = gets.to_i
+
+for i in 1..r do
+  puts "#{i}回目の計算"
+  puts "2つの値を入力してください"
+
+  a = gets.to_i
+  b = gets.to_i
+
+  puts "a=#{a}"
+  puts "b=#{b}"
+
+  puts "計算結果を出力します"
+  puts "#{a}+#{b}=#{a + b}"
+  puts "#{a}-#{b}=#{a - b}"
+  puts "#{a}*#{b}=#{a * b}"
+  puts "#{a}/#{b}=#{a / b}"
+
+end
+
+puts "計算を終了します"
+
+
+# お手本の解答
+=begin
+
+input = gets.to_i
+
+i= 1
+while i <= input do
+  puts "#{i}回目の計算"
+  puts #2つの値を入力してください"
+
+  a = gets.to_i
+  b = gets.to_i
+
+  puts "a=#{a}"
+  puts "b=#{b}"
+
+  puts "計算結果を出力します"
+  puts "#{a}+#{b}=#{a + b}"
+  puts "#{a}-#{b}=#{a - b}"
+  puts "#{a}*#{b}=#{a * b}"
+  puts "#{a}/#{b}=#{a / b}"
+
+  i += 1
+end
+
+puts "計算を終了します"
+
+=end
