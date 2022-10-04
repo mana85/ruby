2c3aafb6d8f0ad0f0e9e5dc8ef5b95d5b59c578c
EC2 Default User <ec2-user@ip-172-31-2-105.ap-northeast-1.compute.internal>
2022-10-04 09:28:03 +0000

    [Update]Ruby11復習と演習

--- override.rb
+++ override.rb
@@ -7,7 +7,9 @@ end
 class Bus < Car
   def run(distance)
     super
+    # ↑これで継承元（親）クラスの同名のメソッドを呼び出せる。
     puts "30人を乗せて、走っています。"
+    # 親クラスには無い要素を追加できる。
   end
 end
 
@@ -16,3 +18,32 @@ bus.run(5)
 
 car = Car.new
 car.run(5)
+
+class Animal
+  def walk
+    puts "四足歩行です"
+  end
+end
+
+class Ostrich < Animal
+  def walk
+    puts "二足歩行です"
+  end
+end
+
+ostrich = Ostrich.new
+ostrich.walk
+# 二足歩行ですが出力される
+
+class Gorilla < Animal
+  def walk
+    super
+    puts "たまに二足歩行もします"
+  end
+end
+
+gorilla = Gorilla.new
+gorilla.walk
+
+
+
